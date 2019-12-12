aaca49f221d130c728a7f83fe1ef3764a4cbec04
Kai Shoya <s-kai@lumber-mill.co.jp>
2019-12-12 18:30:42 +0900

    php版からナビゲーションバーを移植

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,51 +1,57 @@
 <template>
   <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
+    <!-- <nav class="navbar is-dark">
       <div class="navbar-brand">
-        <a
-          class="navbar-item"
-          href="/"
-        >
-          <img
-            src="~assets/buefy.png"
-            alt="Buefy"
-            height="28"
-          >
-        </a>
+        <div class="navbar-item">Sweety</div>
+        <nuxt-link class="navbar-item" to="/" exact-active-class="is-active">価格検索</nuxt-link>
+        <nuxt-link class="navbar-item" to="price" exact-active-class="is-active">ホテル一覧</nuxt-link>
+        <nuxt-link class="navbar-item" to="tos" exact-active-class="is-active">利用規約</nuxt-link>
+      </div>
+    </nav>-->
 
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <div class="navbar-item">Sweety</div>
+        <nuxt-link class="navbar-item" to="/" exact-active-class="is-active">価格検索</nuxt-link>
+        <nuxt-link class="navbar-item" to="price" exact-active-class="is-active">ホテル一覧</nuxt-link>
+
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navMenu"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu" id="navMenu">
+        <div class="navbar-start">
+          <nuxt-link class="navbar-item" to="tos" exact-active-class="is-active">利用規約</nuxt-link>
+          <!-- <nuxt-link class="navbar-item" to="availability" exact-active-class="is-active">空室状況</nuxt-link> -->
+        </div>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="field is-grouped">
+              <p class="control">
+                <!-- <div class="navbar-item">
+                  <span></span>
+                </div> -->
+                <a class="button is-primary" href="">
+                  <span>ログイン</span>
+                </a>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
 
-    <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
-          General
-        </p>
-        <ul class="menu-list">
-          <li
-            v-for="(item, key) of items"
-            :key="key"
-          >
-            <nuxt-link
-              :to="item.to"
-              exact-active-class="is-active"
-            >
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
-
-      <div class="container column is-10">
+    <section class="main-content">
+      <div class="container">
         <nuxt />
       </div>
     </section>
@@ -53,22 +59,16 @@
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      items: [
-        {
-          title: 'Home',
-          icon: 'home',
-          to: { name: 'index' }
-        },
-        {
-          title: 'Inspire',
-          icon: 'lightbulb',
-          to: { name: 'inspire' }
-        }
-      ]
-    }
+document.addEventListener('DOMContentLoaded', () => {
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach(el => {
+      el.addEventListener('click', () => {
+        const target = document.getElementById(el.dataset.target);
+        el.classList.toggle('is-active');
+        target.classList.toggle('is-active');
+      });
+    });
   }
-}
+});
 </script>
