a81581b64edb00fd38a290b5885d206e5612f530
Kai Shoya <s-kai@lumber-mill.co.jp>
2019-12-13 17:23:51 +0900

    ホテル一覧表示画面を作成 APIが未完成のため、一旦jsonを読み込んで対応します。

---
+++ components/HotelCard.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <article class="media">
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <span
+              v-bind:class="[availability == 1 ? 'is-success' : availability == 2 ? 'is-danger' : 'is-warning', 'tag']"
+            >
+              <small v-if="updated_at_availability == null">未更新</small>
+              <small v-else>{{ updated_at_availability }}</small>
+            </span>
+            <span v-if="credit == true" class="tag is-info">
+              <small>クレカOK</small>
+            </span>
+            <br />
+            <a
+              :href="`https://www.google.com/maps/search/?api=1&query=${ address }`"
+              target="_blank"
+              rel="noopener"
+            >
+              <strong style="font-size: 1.2rem;line-hight: 10px;">{{ name }}</strong>
+            </a>
+            <a v-if="website" :href="`${website}`" target="_blank" rel="noopener">{{ website }}</a>
+            <span v-else>{{ website }}</span>
+            <br />
+            <small>{{ address }}</small>
+            <small>
+              <a :href="`tel:${ phone }`">{{ phone }}</a>
+            </small>
+          </p>
+        </div>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    address: {
+      type: String,
+      required: true
+    },
+    phone: {
+      type: String,
+      required: true
+    },
+    availability: {
+      type: Number
+    },
+    updated_at_availability: {
+      type: String
+    },
+    credit: {
+      type: Boolean,
+      required: true
+    },
+    website: {
+      type: String
+    }
+  }
+}
+</script>
---
+++ pages/hotel.vue
@@ -0,0 +1,72 @@
+<template>
+  <section class="section">
+    <div>
+      空き状況：
+      <span class="tag is-success">あり</span>
+      <span class="tag is-danger">なし</span>
+      <span class="tag is-warning">不明</span>
+    </div>
+    <div class="box" v-for="hotel in hotels">
+      <card
+        :name="hotel.name"
+        :address="hotel.address"
+        :phone="hotel.phone"
+        :availability="hotel.availability"
+        :updated_at_availability="hotel.updated_at_availability"
+        :credit="hotel.credit"
+        :website="hotel.website"
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+import card from '@/components/HotelCard'
+import hotels from '@/assets/hotels.json'
+// import prices from '@/assets/prices.json'
+
+export default {
+  components: {
+    card
+  },
+  data () {
+    return {
+      hotels: hotels
+    }
+  },
+  created () {
+    // console.log(this.hotels)
+    // this.$axios.$get('/api?type=hotel').then((res: any) => {
+    //   console.log(res.data)
+    //   // res.data.forEach((value, i) => {
+    //   //   this.hotels.push({
+    //   //     id: value['id'],
+    //   //     name: value['name'],
+    //   //     address: value['address'],
+    //   //     phone: value['phone'],
+    //   //     mapcode: value['mapcode'],
+    //   //     lat: value['lat'],
+    //   //     lon: value['lon'],
+    //   //     credit: value['credit'],
+    //   //     website: value['website'],
+    //   //     created_at: value['created_at'],
+    //   //     updated_at: value['updated_at'],
+    //   //     availability: value['availability'],
+    //   //     updated_at_availability:
+    //   //       value.updated_at_availability == null
+    //   //         ? null
+    //   //         : new Date(value.updated_at_availability)
+    //   //   })
+    //   // })
+    // })
+  }
+}
+</script>
+
+<style scoped>
+.box {
+  padding: 1rem;
+  margin: 0.5rem !important;
+  /* margin-bottom: 0.5rem !important; */
+}
+</style>
